44b182b537c54b43f0daf0c4e6794c074e5d0e25
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-22 16:36:26 -0300

    test: create vehicle

---
+++ server/test/vehicle.test.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const router = require("../src/app");
+const supertest = require("supertest");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(router);
+
+const request = supertest(app.listen(3002, () => {
+    console.log("Vehicle tests...");
+}));
+const mainUser = {
+    name: `${Date.now()}`,
+    email: `${Date.now()}@hotmail.com`,
+    password: `${Date.now()}`
+};
+let authToken = "";
+const mainVehicle = {
+    model:`model-${Date.now()}`,
+    brand:`brand-${Date.now()}`,
+    year:`${Date.now()}`,
+    color:`color-${Date.now()}`,
+    chassis:`chassis-${Date.now()}`,
+    price:`${Date.now()}`,
+    licensePlate:`licence-${Date.now()}`
+};
+
+beforeAll(() => {
+    return request.post("/user")
+        .send(mainUser)
+        .then(res => {
+            return request.post("/auth")
+                .send({ email: mainUser.email, password: mainUser.password })
+                .then(res => {
+                    authToken = res.body.token;
+                });
+        });
+});
+
+describe("Vehicle test suite",()=>{
+    test("Should register a new vehicle",()=>{
+        return request.post("/vehicle")
+            .set({"authorization":authToken})
+            .send(mainVehicle)
+            .then(res=>{
+                expect(res.statusCode).toEqual(201);
+            })
+            .catch(error=>{
+                fail(error);
+            });
+    });
+});
