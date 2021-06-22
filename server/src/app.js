b124be9d830f1e7d72227c5442ccb02723f65d6c
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-22 13:19:55 -0300

    feat:  endpoint user create

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const UserController = require("./controllers/UserController");
 
 router.use((req,res,next)=>{
     next();
 });
 
+router.post("/user",UserController.create);
 module.exports = router;
