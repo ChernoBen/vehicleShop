c54801093ae030f055a187f82062f8440b0ed586
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-22 13:17:34 -0300

    fix: connect

--- server/src/database.js
+++ server/src/database.js
@@ -1,6 +1,6 @@
 const keys = require("../keys");
 const mongoose =  require("mongoose");
-mongoose.connection(`mongodb://${keys.mgHost}:${keys.mgPort}/${keys.mgDatabase}`,{userNewUrlParser: true,useUnifiedTopology:true})
+mongoose.connect("mongodb://localhost:27017/carshop",{userNewUrlParser: true,useUnifiedTopology:true})
     .then(()=>{
         console.log("server running")
     })
