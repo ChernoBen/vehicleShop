30c19f33e2fe1ff9e175e94a5def81e310a94005
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-22 12:22:48 -0300

    feat: sales model

---
+++ server/src/models/Sales.js
@@ -0,0 +1,17 @@
+const mongoose = require("../database");
+
+const SalesSchema = new mongoose.Schema({
+    date: String,
+    price: String,
+    vehicleId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Vehicle",
+        required: true
+    },
+    saller:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Seller",
+        required:true
+    },
+    commission:String
+});
