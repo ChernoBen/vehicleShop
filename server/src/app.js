e59313ed8055dfb656987b0286ccbc6e1c185c6c
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-22 16:39:05 -0300

    feat: vehicle create & user auth endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("./controllers/UserController");
+const VehicleController = require("./controllers/VehicleController");
 
 router.use((req,res,next)=>{
     next();
 });
 
+//user
 router.post("/user",UserController.create);
+router.post("/auth",UserController.auth);
+//vehicle
+router.post("/vehicle",VehicleController.create);
 module.exports = router;
