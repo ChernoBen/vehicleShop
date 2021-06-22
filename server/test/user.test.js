2ac0851a5582dd9b198390efa2f9362c9b3b7937
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-22 16:36:47 -0300

    test: user validation

--- server/test/user.test.js
+++ server/test/user.test.js
@@ -28,4 +28,91 @@ describe("User test suites",()=>{
                 fail(error);
             });
     });
+    test("Should prevent empty data entry by user .", () => {
+		let user = {
+			name: "",
+			email: "",
+			password: ""
+		};
+		return request.post("/user")
+			.send(user)
+			.then(res => {
+				expect(res.statusCode).toEqual(400);
+			})
+			.catch(error => {
+				fail(error);
+			});
+	});
+
+	test("Should prevent a registration of an email or cpf already registered .", () => {
+		let time = Date.now();
+		let email = `${time}@gmail.com`;
+		let user = {
+			name: "benjamim",
+			email: email,
+			password: `${Date.now()}`
+		};
+		return request.post("/user")
+			.send(user)
+			.then(res => {
+				expect(res.statusCode).toEqual(201);
+				expect(res.body.email).toEqual(email);
+				return request.post("/user")
+					.send(user)
+					.then(res => {
+						expect(res.statusCode).toEqual(400);
+					})
+					.catch(error => {
+						fail(error);
+					});
+			})
+			.catch(error => {
+				fail(error);
+			});
+	});
+    test("Should return a auth token .", () => {
+		return request.post("/auth")
+			.send({ email: mainUser.email, password: mainUser.password })
+			.then(res => {
+				expect(res.statusCode).toEqual(200);
+				expect(res.body.token).toBeDefined();
+			})
+			.catch(error => {
+				fail(error);
+			});
+	});
+
+	test("Should prevent an unregistered person to get an auth token.", () => {
+		return request.post("/auth")
+			.send({ email: "false@email.com", password: mainUser.password })
+			.then(res => {
+				expect(res.statusCode).toEqual(403);
+			})
+			.catch(error => {
+				fail(error);
+			});
+	});
+
+	test("Should prevent from entering an incorrect password.", () => {
+		return request.post("/auth")
+			.send({ email: mainUser.email, password: "wrongPassword" })
+			.then(res => {
+				expect(res.statusCode).toEqual(403);
+			})
+			.catch(error => {
+				fail(error);
+			});
+	});
+
+	test("Should update a user with success", () => {
+		return request.post("/auth")
+			.send({ email: mainUser.email, password: mainUser.password })
+			.then(res => {
+				authToken = res.body.token
+				expect(res.statusCode).toEqual(200);
+				expect(res.body.token).toBeDefined();
+			}).catch(error => {
+				fail(error);
+			});
+	});
 });
