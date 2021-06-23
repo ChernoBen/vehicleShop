e43d31838c44470bf69d7a7b40bf9d1fb959962b
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-23 12:36:56 -0300

    test: create sale

---
+++ server/test/sales.test.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const router = require("../src/app");
+const supertest = require("supertest");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(router);
+
+const request = supertest(app.listen(3004, () => {
+    console.log("Sales tests...");
+}));
+const mainUser = {
+    name: `${Date.now()}`,
+    email: `${Date.now()}@hotmail.com`,
+    password: `${Date.now()}`
+};
+let authToken = "";
+let vehicle = "";
+let seller = "";
+
+beforeAll(() => {
+    return request.post("/user")
+        .send(mainUser)
+        .then(res => {
+            return request.post("/auth")
+                .send({ email: mainUser.email, password: mainUser.password })
+                .then(res => {
+                    authToken = res.body.token;
+                });
+        });
+});
+
+describe("Sales test suite",()=>{
+    test("Should create a new sale",()=>{
+        return request.get("/vehicle")
+            .then(res=>{
+                expect(res.statusCode).toEqual(200);
+                vehicle = res.body[0];
+                return request.get("/seller")
+                    .then(res=>{
+                        expect(res.statusCode).toEqual(200);
+                        seller = res.body[0];
+                        return request.post("/sales")
+                            .set({"authorization":authToken})
+                            .send({
+                                vehicleId:vehicle._id,
+                                seller:seller._id,
+                                price:vehicle.price,
+                                date:Date.now()
+                            })
+                            .then(res=>{
+                                expect(res.statusCode).toEqual(201);
+                            })
+                            .catch(error=>{
+                                fail(error);
+                            });
+                    })
+                    .catch(error=>{
+                        fail(error);
+                    });
+            })
+            .catch(error=>{
+                fail(error);
+            });
+    });
+});
