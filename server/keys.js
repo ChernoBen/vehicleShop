91eba666167bfd8c7f725bd4484ad72cacf3546d
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-23 17:48:36 -0300

    env: setting env vars

--- server/keys.js
+++ server/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     apiPort:process.env.API_PORT,
-    secret:"lksjdhflakjdslçkasjdçj123",
+    secret:process.env.SECRET,
     mgHost: process.env.HOST,
     mgDatabase: process.env.MONGO_INITDB_DATABASE,
     mgPassword: process.env.ME_CONFIG_MONGODB_ADMINPASSWORD,
