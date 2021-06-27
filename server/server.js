787a883a4dcb82ae60211d22074a010d3e7e001c
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-27 17:06:41 -0300

    refactor: change api port

--- server/server.js
+++ server/server.js
@@ -6,18 +6,22 @@ const keys = require("./keys");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger_output.json");
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors("*"));
+
+app.use(cors({
+	origin: '*',
+	optionsSuccessStatus: 200
+}));
 const options = {
 	customCss: ".swagger-ui .topbar { display: none }",
 	customSiteTitle: "CarShop",
-	explorer:true
+	explorer: true
 };
 
-app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile,options));
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile, options));
 app.use(routes);
 
-app.listen(keys.apiPort,()=>{
-    console.log("...server running");
+app.listen(5000,() => {
+	console.log("...server running");
 });
